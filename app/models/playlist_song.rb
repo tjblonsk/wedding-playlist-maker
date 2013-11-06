29219b5f2b653a5f7e8047b0b416c7b7068e8be7
primathinks <primathinks@gmail.com>
2013-11-06 16:59:33 -0500

    Added model associations

--- app/models/playlist_song.rb
+++ app/models/playlist_song.rb
@@ -1,2 +1,5 @@
 class PlaylistSong < ActiveRecord::Base
+	belongs_to :playlist
+	belongs_to :song
+	delegate :name, :artist, :album, :duration, :thumb60, :audio, :to => :song, :allow_nil => true
 end
--- app/models/songs.rb
+++ app/models/songs.rb
@@ -1,2 +1,4 @@
 class Songs < ActiveRecord::Base
+	has_many :playlist_songs
+	has_many :playlists, through: :playlist_songs
 end
