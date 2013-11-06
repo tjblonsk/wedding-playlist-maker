1745e872044cc038bee9fd0cebefa67564094b9a
primathinks <primathinks@gmail.com>
2013-11-06 12:02:40 -0500

    Initialized project and added Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeddingPlaylistMaker::Application.config.secret_key_base = 'a4b97e36a8dd33272c2374c28f41ee4a1c27bef4f33401e1a04077f7e719420add2f4ce45ed042242bba33b28c55b0ead8b371e73667fc62bbdb6ce92394c76a'
