76f3a7f50b7fab9900df6d4d52f090192584b663
primathinks <primathinks@gmail.com>
2013-11-07 11:21:18 -0500

    Destroy all playlist records and their dependent records

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,6 @@
 class Playlist < ActiveRecord::Base
 	validates_presence_of :name, presence: true
 	belongs_to :user
-	has_many :playlist_songs
+	has_many :playlist_songs, dependent: :destroy
 	has_many :songs, through: :playlist_songs
 end
