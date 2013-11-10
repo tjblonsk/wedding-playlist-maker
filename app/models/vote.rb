6feb766325ce34c8e8bb2373b5a0748d73d824ac
primathinks <primathinks@gmail.com>
2013-11-09 23:48:13 -0500

    vote DRYer

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,11 @@ class Vote < ActiveRecord::Base
 	belongs_to :user
 	
 	def self.find_create(song_id, user_id)
-    	where(:song_id => song_id, :user_id => user_id).first_or_create
+    	where(song_id: song_id, user_id: user_id).first_or_create
+  	end
+
+  	def self.find_current_vote(song_id, user_id)
+  		current_vote = self.where(song_id: song_id, user_id: user_id).first
   	end
 
 end
