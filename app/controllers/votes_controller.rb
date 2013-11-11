e292d2727409b2e218467db0d3b1e5b362b58f11
primathinks <primathinks@gmail.com>
2013-11-11 16:18:44 -0500

    Added pagination to votes page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,6 +3,8 @@ class VotesController < ApplicationController
     @playlists = Playlist.find_current(current_user.id)
     @playlist_songs = PlaylistSong.by_playlist(@playlists).collect {|x| x.song_id}
     @eligible_songs = Vote.find_eligible(@playlist_songs)
+    @eligible_songs.uniq! {|s| s.song_id }
+    @paged_songs = Kaminari.paginate_array(@eligible_songs).page(params[:page]).per(10)
   end
 
   def create
