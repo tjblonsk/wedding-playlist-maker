e523de395594c18ea6b99ca9ddcd1eacd448e258
primathinks <primathinks@gmail.com>
2013-11-11 11:33:18 -0500

    Results are now sorted by most popular

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,6 @@
 class ResultsController < ApplicationController
   def index
-  	#@results = Vote.find_by_sql "Select *, count(cast_vote) AS votings FROM votes GROUP BY song_id ORDER BY votings DESC"
-  	@results = Vote.find_by_sql "Select *, count(cast_vote) AS votings FROM votes WHERE cast_vote = 2 GROUP BY song_id ORDER BY votings DESC"
+  	vote_category = 1
+  	@results = Vote.find_by_sql "Select *, count(cast_vote) AS votings FROM votes WHERE cast_vote = #{vote_category} GROUP BY song_id ORDER BY votings DESC"
   end
 end
