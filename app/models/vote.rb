3ece833064ec10d04a586128868efab809c9a6f6
primathinks <primathinks@gmail.com>
2013-11-14 10:16:35 -0500

    Fixed ordering of votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,14 +2,14 @@ class Vote < ActiveRecord::Base
   	belongs_to :song
   	belongs_to :user
 
-    delegate :name, :artist, :album, :duration, :thumb60, :audio, :to => :song, :allow_nil => true
+    delegate :name, :artist, :album, :duration, :thumb60, :audio, :created_at, :to => :song, :allow_nil => true
   	
   	def self.find_create(song_id, user_id)
     	where(song_id: song_id, user_id: user_id).first_or_create
   	end
 
     def self.find_eligible(user_songs)
-      where.not(song_id: user_songs).to_a
+      where.not(song_id: user_songs).order(updated_at: :desc).to_a
     end
 
   	def self.find_current_vote(song_id, user_id)
